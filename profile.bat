75a37cbca8e841b142c2925e4925a4552d32a54d
Andrew <abberger1@gmail.com>
2018-02-04 16:15:25 -0600

    fixed batch

--- profile.bat
+++ profile.bat
@@ -1,14 +1,14 @@
 
 cd bin
-more ..\tests\tree.c | assign1-0.exe 
+more ..\tests\tree.txt | assign1-0.exe 
 gprof assign1-0.exe gmon.out > ..\data\gprof-assign1-0-tree.txt
 
-more ..\tests\list.c | assign1-0.exe 
+more ..\tests\list.txt | assign1-0.exe 
 gprof assign1-0.exe gmon.out > ..\data\gprof-assign1-0-list.txt
 
-more ..\tests\tree.c | assign1-2.exe 
+more ..\tests\tree.txt | assign1-2.exe 
 gprof assign1-2.exe gmon.out > ..\data\gprof-assign1-2-tree.txt
 
-more ..\tests\list.c | assign1-2.exe 
+more ..\tests\list.txt | assign1-2.exe 
 gprof assign1-2.exe gmon.out > ..\data\gprof-assign1-2-list.txt
 
